821a68c51c46a5f9902c125600444ffcb18b57fa
Saniya Memon <saniyamemon7862@gmail.com>
2022-01-17 10:51:37 -0500

    checkout form completed!

--- TeaWorld/app/src/main/java/com/example/teaworld/checkout_form.java
+++ TeaWorld/app/src/main/java/com/example/teaworld/checkout_form.java
@@ -2,7 +2,11 @@ package com.example.teaworld;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class checkout_form extends AppCompatActivity {
 
@@ -10,5 +14,22 @@ public class checkout_form extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_checkout_form);
+        Button btn2=findViewById(R.id.button2);
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(getApplicationContext(),checkout_form.class);
+                startActivity(i);
+            }
+        });
+
+        Button btn1=findViewById(R.id.button);
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText( ,"Order placed successfully.", Toast.LENGTH_SHORT).show();
+                Toast.makeText( "", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
