dd66fdb472d4901458ea7b4407c18ffc021d8fc4
Saniya Memon <saniyamemon7862@gmail.com>
2022-01-18 16:54:10 -0500

    Product list activity completed!

--- TeaWorld2/app/src/main/java/com/example/teaworld/MainActivity.java
+++ TeaWorld2/app/src/main/java/com/example/teaworld/MainActivity.java
@@ -2,13 +2,43 @@ package com.example.teaworld;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    public void callingAnotherActivity(){
+        Intent i=new Intent(getApplicationContext(),checkout_form.class);
+        startActivity(i);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button turkishBtn=findViewById(R.id.turkish_buy);
+        Button tandoorBtn=findViewById(R.id.tandoor_buy);
+        Button indianBtn=findViewById(R.id.indian_buy);
+
+        turkishBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                callingAnotherActivity();
+            }
+        });
+        tandoorBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                callingAnotherActivity();
+            }
+        });
+        indianBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                callingAnotherActivity();
+            }
+        });
+
     }
 }
---
+++ TeaWorld2/app/src/main/java/com/example/teaworld/checkout_form.java
@@ -0,0 +1,14 @@
+package com.example.teaworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class checkout_form extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_checkout_form);
+    }
+}
