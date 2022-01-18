7389dde28fedcff8d3132dafe2058e044911a131
Saniya Memon <saniyamemon7862@gmail.com>
2022-01-18 14:46:45 -0500

    Another application of Tea created

--- TeaWorld/app/src/main/java/com/example/teaworld/MainActivity.java
+++ TeaWorld/app/src/main/java/com/example/teaworld/MainActivity.java
@@ -10,11 +10,12 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    @SuppressLint("WrongViewCast")
+    //@SuppressLint("WrongViewCast")
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.product_list);
+
         TextView buy1 = findViewById(R.id.buy1);
          buy1.setOnClickListener(new View.OnClickListener() {
              @Override
@@ -24,14 +25,16 @@ public class MainActivity extends AppCompatActivity {
              }
          });
 
-        TextView buy2 = findViewById(R.id.buy2);
-        buy2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i=new Intent(getApplicationContext(),checkout_form.class);
-                startActivity(i);
-            }
-        });
+
+//        TextView buy2 = findViewById(R.id.buy2);
+//        buy2.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent i=new Intent(getApplicationContext(),checkout_form.class);
+//                startActivity(i);
+//            }
+//        });
+
         TextView buy3 = findViewById(R.id.buy3);
         buy3.setOnClickListener(new View.OnClickListener() {
             @Override
--- TeaWorld/app/src/main/java/com/example/teaworld/checkout_form.java
+++ TeaWorld/app/src/main/java/com/example/teaworld/checkout_form.java
@@ -2,11 +2,7 @@ package com.example.teaworld;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
 
 public class checkout_form extends AppCompatActivity {
 
@@ -14,21 +10,5 @@ public class checkout_form extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_checkout_form);
-        Button btn2=findViewById(R.id.button2);
-        btn2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i=new Intent(getApplicationContext(),MainActivity.class);
-                startActivity(i);
-            }
-        });
-
-        Button btn1=findViewById(R.id.button);
-        btn2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText( getApplicationContext(),"Order placed successfully.", Toast.LENGTH_SHORT).show();
-            }
-        });
     }
 }
