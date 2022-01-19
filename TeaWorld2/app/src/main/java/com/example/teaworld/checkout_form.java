6dcad4957cdf0d26d0d3358160e49d57c86ce144
Saniya Memon <saniyamemon7862@gmail.com>
2022-01-18 20:17:59 -0500

    Place order button created

--- TeaWorld2/app/src/main/java/com/example/teaworld/checkout_form.java
+++ TeaWorld2/app/src/main/java/com/example/teaworld/checkout_form.java
@@ -2,7 +2,11 @@ package com.example.teaworld;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class checkout_form extends AppCompatActivity {
 
@@ -10,5 +14,20 @@ public class checkout_form extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_checkout_form);
+        Button placeOrder = findViewById(R.id.placeOrder);
+        placeOrder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Order placed succesfully!", Toast.LENGTH_SHORT).show();
+            }
+        });
+        Button back = findViewById(R.id.back);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), checkout_form.class);
+                startActivity(i);
+            }
+        });
     }
 }
